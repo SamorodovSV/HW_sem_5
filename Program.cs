c14bb06e06ba9baf1028e679eb87b488a657120f
Samorodov Sergey <samorodovsv@yandex.ru>
2022-07-13 12:08:33 +0300

    Добавили 2 задачу

--- Program.cs
+++ Program.cs
@@ -3,6 +3,111 @@
 // [345, 897, 568, 234] -> 2
 
 
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         int[] CreateRandomArray(int size, int min, int max)
+//         {
+//             int[] array = new int[size];
+//             for (int i = 0; i < size; i++)
+//             {
+//                 array[i] = new Random().Next(min, max + 1);
+//             }
+//             return array;
+//         }
+
+//         void ShowArray(int[] array)
+//         {
+
+//             for (int i = 0; i < array.Length; i++)
+//             {
+//                 Console.Write(array[i] + " ");
+//             }
+//             Console.WriteLine();
+//         }
+//         Console.Clear();
+//         Console.Write("Введите количество элементов массива: ");
+//         int size = Convert.ToInt32(Console.ReadLine());
+//         int[] myArray = CreateRandomArray(size , 100, 999);
+//         ShowArray(myArray);
+//         int N = 0;
+//         for (int i = 0; i < myArray.Length; i++)
+//         {
+//             if (myArray[i] % 2 == 0)
+//             {
+//                 N++;
+//             }
+//             else
+//             {
+//                 N = N + 0;
+//             }
+//         }
+//         Console.WriteLine($"Количество четных чисел: {N}");
+//     }
+// }
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// internal class Program
+// {
+//     private static void Main(string[] args)
+//     {
+//         int[] CreateRandomArray(int size, int min, int max)
+//         {
+//             int[] array = new int[size];
+//             for (int i = 0; i < size; i++)
+//             {
+//                 array[i] = new Random().Next(min, max + 1);
+//             }
+//             return array;
+//         }
+
+//         void ShowArray(int[] array)
+//         {
+
+//             for (int i = 0; i < array.Length; i++)
+//             {
+//                 Console.Write(array[i] + " ");
+//             }
+//             Console.WriteLine();
+//         }
+//         Console.Clear();
+//         Console.Write("Введите количество элементов массива: ");
+//         int num = Convert.ToInt32(Console.ReadLine());
+//          Console.Write("Введите минимальное число: ");
+//         int start = Convert.ToInt32(Console.ReadLine());
+//          Console.Write("Введите максимальное число: ");
+//         int end = Convert.ToInt32(Console.ReadLine());
+//         int[] myArray = CreateRandomArray(num , start, end);
+//         ShowArray(myArray);
+//         int sum = 0;
+//         for (int i = 0; i < myArray.Length; i++)
+//         {
+//             if (i % 2 == 0)
+//             {
+//                 sum = sum + 0;
+//             }
+//             else
+//             {
+//                 sum = sum + myArray[i];
+//             }
+//         }
+//         Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sum}");
+//     }
+// }
+
+
+
+// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
 internal class Program
 {
     private static void Main(string[] args)
@@ -26,22 +131,27 @@ internal class Program
             }
             Console.WriteLine();
         }
+        Console.Clear();
         Console.Write("Введите количество элементов массива: ");
-        int size = Convert.ToInt32(Console.ReadLine());
-        int[] myArray = CreateRandomArray(size , 100, 999);
+        int num = Convert.ToInt32(Console.ReadLine());
+         Console.Write("Введите минимальное число: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+         Console.Write("Введите максимальное число: ");
+        int end = Convert.ToInt32(Console.ReadLine());
+        int[] myArray = CreateRandomArray(num , start, end);
         ShowArray(myArray);
-        int N = 0;
+        int sum = 0;
         for (int i = 0; i < myArray.Length; i++)
         {
-            if (myArray[i] % 2 == 0)
+            if (i % 2 == 0)
             {
-                N++;
+                sum = sum + 0;
             }
             else
             {
-                N = N + 0;
+                sum = sum + myArray[i];
             }
         }
-        Console.WriteLine($"Количество четных чисел: {N}");
+        Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {sum}");
     }
 }
